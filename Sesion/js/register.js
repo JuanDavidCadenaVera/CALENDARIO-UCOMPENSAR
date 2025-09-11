d972115935eb7d74e117dab3a46c3d7ee8eb163b
Juan David Cadena Vera <juandavidcadenavera@hotmail.com>
2025-09-11 12:11:38 -0500

    Queda subido el register js funcional

--- Sesion/js/register.js
+++ Sesion/js/register.js
@@ -11,7 +11,7 @@ document.getElementById("formRegistro").addEventListener("submit", async (e) => 
   }
 
   try {
-    const response = await fetch("http://localhost:3000/enviar-email", {
+    const response = await fetch("https://backend-calendario-ucompensar.onrender.com/enviar-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
